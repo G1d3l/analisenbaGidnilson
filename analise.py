ad3ef811fc6ed1db9693c058de47f73418bf86ca
G1d3l <129759849+G1d3l@users.noreply.github.com>
2025-06-06 20:30:11 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,3 +1,4 @@
+#bibliotecas necessarias
 !pip install pyspark
 !pip install pandas numpy
 !pip install matplotlib seaborn plotly
@@ -6,7 +7,7 @@
 !pip install pyspark pandas matplotlib seaborn plotly
 
 
-
+#claramente importações 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, round
 import pandas as pd
@@ -14,10 +15,10 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+# Criação da SparkSession
 spark = SparkSession.builder.appName("NBAplayers").getOrCreate()
 
-
+# dados de jogadores
 data = [
     ("LeBron James", "LAL", 1698, 514, 488, 55, 2022),
     ("Stephen Curry", "GSW", 1873, 469, 354, 64, 2022),
@@ -30,33 +31,34 @@ data = [
     ("Jimmy Butler", "MIA", 1500, 370, 420, 55, 2022),
     ("Devin Booker", "PHX", 1700, 390, 380, 58, 2022)
 ]
-
+# nomes das colunas 
 columns = ["jogador", "Time", "pontos", "Assist", "rebotes", "GP", "Temporada"]
-
+# Criação do DataFrame Spark
 df = spark.createDataFrame(data, columns)
-
+#calculo de metricas
 df = df.withColumn("PPG", round(col("pontos") / col("GP"), 2))
 df = df.withColumn("APG", round(col("assist") / col("GP"), 2))
 df = df.withColumn("RPG", round(col("rebotes") / col("GP"), 2)) 
 df = df.withColumn("EFF", round((col("pontos") + col("rebotes") + col("assist")) / col("GP"), 2))
 
-
+#Conversão para DataFrame Pandas para visualização com Plotly e Seaborn
 df_pd = df.toPandas()
 
-
+#grafico 1
 fig1 = px.bar(df_pd, x="jogador", y="PPG", title="Pontos por Jogo (PPG)", color="jogador",
               labels={"PPG": "Pontos por Jogo", "jogador": "Jogador"})
 fig1.show()
 
-
+#grafico 2
 fig2 = px.bar(df_pd, x="jogador", y="EFF", title="Eficiência (EFF)", color="jogador",
               labels={"EFF": "Eficiência", "jogador": "Jogador"})
 fig2.show()
-
+#grafico 3
 fig3 = px.scatter(df_pd, x="APG", y="RPG", color="jogador", title=" Assistências vs Rebotes",
                   labels={"APG": "Assistências por Jogo (APG)", "RPG": "Rebotes por Jogo (RPG)"})
 fig3.show()
 
+#Boxplot para visualizar a distribuição das estatísticas
 plt.figure(figsize=(10, 6))
 sns.boxplot(data=df_pd[["PPG", "APG", "RPG", "EFF"]])
 plt.title(" Distribuição das Estatísticas (PPG, APG, RPG, EFF)")
